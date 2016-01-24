3fb3fdb262a952c8c4c2c5e409644845d40b4e5e
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-01-24 11:51:25 +0200

    Polymorphic user data / toObject

--- src/Web/Users/Remote.hs
+++ src/Web/Users/Remote.hs
@@ -1,44 +1,56 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Web.Users.Remote where
 
 import           Control.Monad.IO.Class
 
-import           Data.Aeson.TH
+import           Data.Aeson
 import           Data.Int
 import           Data.MessagePack.Object
-import qualified Data.Text as T
+import           Data.Proxy
+import qualified Data.Text                    as T
+import qualified Data.Vector                  as V
 
 import           Database.PostgreSQL.Simple
 
 import           Network.MessagePack.Server
 
-import           Web.Users.Types
-import           Web.Users.Postgresql ()
+import           Web.Users.Types              hiding (UserId)
+import           Web.Users.Postgresql         ()
 
-instance MessagePack Int64 where
+type UserId = Int64
+
+instance MessagePack UserId where
   toObject = undefined
   fromObject = undefined
 
-data UserData = UserData { userCredits :: Int }
-
-instance MessagePack (User UserData) where
-  toObject = undefined
+instance MessagePack Password where
+  toObject (PasswordHash hash) = ObjectArray $ V.fromList
+    [ ObjectBool True, ObjectStr hash ]
   fromObject = undefined
 
-deriveJSON defaultOptions ''UserData
+instance MessagePack a => MessagePack (User a) where
+  toObject User {..} = ObjectArray $ V.fromList
+    [ ObjectStr u_name
+    , ObjectStr u_email
+    , toObject u_password
+    , ObjectBool u_active
+    , toObject u_more
+    ]
+  fromObject = undefined
 
-runServer :: IO ()
-runServer = do
+runServer :: forall a. (MessagePack a, FromJSON a, ToJSON a) => Proxy a -> IO ()
+runServer _ = do
   conn <- connectPostgreSQL ""
   initUserBackend conn
 
-  let getUserIdByName' :: T.Text -> Server (Maybe Int64)
+  let getUserIdByName' :: T.Text -> Server (Maybe UserId)
       getUserIdByName' = liftIO . getUserIdByName conn
 
-      getUserById' :: Int64 -> Server (Maybe (User UserData))
+      getUserById' :: (MessagePack a, FromJSON a, ToJSON a) => UserId -> Server (Maybe (User a))
       getUserById' = liftIO . getUserById conn
 
   serve 8537 [ method "getUserIdByName" getUserIdByName'
