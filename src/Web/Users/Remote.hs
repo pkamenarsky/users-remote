bff94e676b8b41666d581f254723260b1015fe64
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-01-24 12:05:32 +0200

    fromObject

--- src/Web/Users/Remote.hs
+++ src/Web/Users/Remote.hs
@@ -8,6 +8,8 @@ module Web.Users.Remote where
 import           Control.Monad.IO.Class
 
 import           Data.Aeson
+import qualified Data.ByteString.Lazy        as B
+import qualified Data.Binary                 as BI
 import           Data.Int
 import           Data.MessagePack.Object
 import           Data.Proxy
@@ -24,13 +26,19 @@ import           Web.Users.Postgresql         ()
 type UserId = Int64
 
 instance MessagePack UserId where
-  toObject = undefined
-  fromObject = undefined
+  toObject uid = ObjectBin (B.toStrict $ BI.encode uid)
+  fromObject (ObjectBin bin) = case BI.decodeOrFail $ B.fromStrict bin of
+    Right (_, _, a) -> a
+    _ -> Nothing
+  fromObject _ = Nothing
 
 instance MessagePack Password where
   toObject (PasswordHash hash) = ObjectArray $ V.fromList
     [ ObjectBool True, ObjectStr hash ]
-  fromObject = undefined
+  fromObject (ObjectArray a) = case V.toList a of
+    [ ObjectBool True, ObjectStr hash ] -> Just (PasswordHash hash)
+    _ -> Nothing
+  fromObject _ = Nothing
 
 instance MessagePack a => MessagePack (User a) where
   toObject User {..} = ObjectArray $ V.fromList
@@ -40,7 +48,11 @@ instance MessagePack a => MessagePack (User a) where
     , ObjectBool u_active
     , toObject u_more
     ]
-  fromObject = undefined
+  fromObject (ObjectArray a) = case V.toList a of
+    [ ObjectStr u_name , ObjectStr u_email , u_password , ObjectBool u_active , u_more ]
+      -> User <$> pure u_name <*> pure u_email <*> fromObject u_password <*> pure u_active <*> fromObject u_more
+    _ -> Nothing
+  fromObject _ = Nothing
 
 runServer :: forall a. (MessagePack a, FromJSON a, ToJSON a) => Proxy a -> IO ()
 runServer _ = do
